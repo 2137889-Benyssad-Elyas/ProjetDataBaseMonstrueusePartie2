e2f6e15ee043c84ff326fde266205dccf2e7cf2e
Constant Renard <2134914@carrefour.cegepvicto.ca>
2022-05-01 16:22:25 -0400

    essaie ++

--- Fonctions.sql
+++ Fonctions.sql
@@ -1,5 +1,5 @@
 USE ressource_monstrueuse;
-
+DROP FUNCTION IF EXISTS responsable;
 /**
 * Cette fonction décrypte un contenu crypté en utilisant la clé de cryptage définie ci-dessus. Elle retourne le texte clair prêt à être lu par un être humain.
 *
@@ -9,19 +9,21 @@ USE ressource_monstrueuse;
 */
 DELIMITER $$
 
-CREATE FUNCTION decryptage(_donnees_crytees BLOB) RETURNS VARCHAR(255) DETERMINISTIC
+CREATE FUNCTION responsable(_fonction_salle VARCHAR(255), _date_responsable DATETIME) RETURNS INTEGER DETERMINISTIC
 BEGIN
-	DECLARE _nombre_etudiant INTEGER;
-    SET _nombre_etudiant = (
-		SELECT count(etudiant) FROM Inscription
-			INNER JOIN Groupe ON groupe = id_groupe
-            NATURAL JOIN Cours
-            INNER JOIN Session ON session = id_session
-		WHERE semestre = _semestre AND annee = _annee AND sigle = _sigle AND numero_groupe = _numero_groupe
+	DECLARE _id_monstre_responsable INTEGER;
+    SET _id_monstre_responsable = (
+		SELECT monstre, responsabilite FROM Affectation_salle
+            INNER JOIN Salle ON id_salle = salle
+            INNER JOIN Responsabilite ON responsabilite = id_responsabilite
+		WHERE _fonction_salle = fonction AND _date_responsable BETWEEN debut_affectation AND fin_affectation
+        ORDER BY niveau_responsabilite LIMIT 1
 	);
-    
-    RETURN _nombre_etudiant;
+
+    RETURN _id_monstre_responsable;
     
 END $$
 
 DELIMITER ;
+
+SELECT responsable('salle des explosifs', '1082-06-27 04:00:00');
